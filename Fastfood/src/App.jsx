18dd69a079f5e2017e08944a597d245f1613fed3
Andres Paz <andres.paz997@gmail.com>
2025-05-08 00:06:09 -0500

    Problema resuelto

--- Fastfood/src/App.jsx
+++ Fastfood/src/App.jsx
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -34,43 +33,4 @@ function App() {
 }
 
 export default App
-=======
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { ProveedorCarrito } from "./contexto/ContextoCarrito.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <ProveedorCarrito>
-      <App />
-    </ProveedorCarrito>
-  </React.StrictMode>
-);
-
-// App.jsx
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Inicio from "./paginas/Inicio";
-import Recetas from "./paginas/Recetas";
-import DetalleReceta from "./paginas/DetalleReceta";
-import Pago from "./paginas/Pago";
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Recetas />} />
-        <Route path="/inicio" element={<Inicio />} />
-        <Route path="/receta/:id" element={<DetalleReceta />} />
-        <Route path="/carrito" element={<Pago />} />
-      </Routes>
-    </Router>
-  );
-}
-export default App;
-<<<<<<< HEAD
->>>>>>> dev_luis
-=======
->>>>>>> origin/dev_luis
->>>>>>> dev_luis
